feb830972ddacae29eaf42673cf892ab2b54f295
coradoa <acoradop@miumg.edu.gt>
2024-10-08 21:05:02 -0600

    Agregando la config de la nube
    AC

--- config/db.js
+++ config/db.js
@@ -1,9 +1,10 @@
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'Belfast16',
+  host: 'mysql-26f6774d-miumg-96e0.f.aivencloud.com',
+  user: 'avnadmin',
+  port:  '13408',
+  password: 'AVNS_pjw6sbLUfqaRpCza2cQ',
   database: 'salon'
 });
 
